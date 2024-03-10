63063b3cb0ef8658ff363aeb5e5f80112444ef50
vvloi <vvloi92@gmail.com>
2024-03-10 19:56:34 +0700

    add log for advisor

--- web-utils/src/main/java/com/preschool/library/webutils/ControllerAdvisorAbstract.java
+++ web-utils/src/main/java/com/preschool/library/webutils/ControllerAdvisorAbstract.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -15,12 +17,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 public abstract class ControllerAdvisorAbstract extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotAuthorizedException.class)
     public ResponseEntity<Response<Void>> handleLoginUnsuccessful(
             NotAuthorizedException notAuthorizedException, WebRequest request) {
         HttpStatus unauthorized = HttpStatus.UNAUTHORIZED;
+        log.error("Not authorized exception");
         return ResponseEntity.status(unauthorized).body((Response.error(unauthorized.name(), null)));
     }
 
@@ -30,6 +34,8 @@ public abstract class ControllerAdvisorAbstract extends ResponseEntityExceptionH
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request) {
+        List<Map<String, String>> fieldsErrors = fieldsErrors(ex);
+        log.error("ARGUMENT_NOT_VALID: {}", fieldsErrors);
         return new ResponseEntity<>(
                 Response.error("ARGUMENT_NOT_VALID", "Some argument fields not valid", fieldsErrors(ex)),
                 HttpStatus.BAD_REQUEST);
