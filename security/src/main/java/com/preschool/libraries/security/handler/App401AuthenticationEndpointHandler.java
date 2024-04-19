7588a754b1e85e0f6994fcdd8fa280dd958aea0a
vvloi <vvloi92@gmail.com>
2024-04-20 01:12:30 +0700

    test ci

--- security/src/main/java/com/preschool/libraries/security/handler/App401AuthenticationEndpointHandler.java
+++ security/src/main/java/com/preschool/libraries/security/handler/App401AuthenticationEndpointHandler.java
@@ -28,5 +28,6 @@ public class App401AuthenticationEndpointHandler implements AuthenticationEntryP
         Response<Void> error = Response.error(HttpStatus.UNAUTHORIZED);
         new ObjectMapper().writeValue(out, error);
         out.flush();
+        // test change file on CI
     }
 }
